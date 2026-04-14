b82eefe1fe81e0ca71f195150ceed7119b99329a
DexTher22 <markmagyar2003@gmail.com>
2026-04-14 13:39:32 +0200

    számítás m

---
+++ src/main/java/com/example/Cylinder.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Cylinder {
+    
+    public static double calcSurface(double radius, double height) {
+        return (2*Math.PI*Math.pow(radius, 2)) + (2 * Math.PI * radius * height);
+    }
+
+}
